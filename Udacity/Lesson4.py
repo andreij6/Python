7f1ec5e8f944f0b4010603b6060a9df9ee2897ad
Andre Jones <andreij6@gmail.com>
2013-11-07 11:50:52 -0600

    python courses udacity lynda

--- Udacity/Lesson4.py
+++ Udacity/Lesson4.py
@@ -1,2 +1,29 @@
 #Data Structures
 
+index = [['udacity', ['http://udacity.com', 'http://npr.org']],
+         ['computing', ['http://acm.org']]]
+
+#--------third video
+
+def add_to_index(index, keyword, url):
+    for entry in index:
+        if entry[0] == keyword:
+            entry[1].append(url)
+            return
+    index.append([keyword,[url]])
+
+
+
+def lookup(index,keyword):
+    for item in index:
+        if item[0] == keyword:
+            return item[1]
+    return []
+    
+def add_page_to_index(index, url, content):
+    keys = content.split()
+    for keyword in keys:
+        add_to_index(index, keyword, url)
+        
+    
+    
