2e1780c7e5d7baab3c81f11af26eed4f8b459a68
Andre Jones <andreij6@gmail.com>
2013-11-12 17:14:50 -0600

    make_hashtable

--- Udacity/CompSci1/Lesson5.py
+++ Udacity/CompSci1/Lesson5.py
@@ -16,4 +16,39 @@ one_percent = estimat * .01
 
 #print one_percent
 
-#-----------
+#----------- 
+
+def hash_string(keyword, buckets):
+    h = 0
+   
+    for c in keyword:
+        h = ( h+ ord(c) )% buckets
+    return h
+    
+    
+
+#print hash_string('a',12)       #should be 1
+
+#print hash_string('udacity',12) #should be 12
+
+#print hash_string('a', 12)
+keyword = "udacity"
+
+#for c in keyword:
+#    print c
+ 
+#----------Creating an Empty Hash Table
+
+def make_hashtable(nbuckets):
+    i = 0
+    table = []
+    while i < nbuckets:
+        table.append([])
+        i = i + 1
+    return table
+
+def better_make_hash(nbuckets):
+    table = []
+    for unused in range(0, nbuckets):
+        table.append([])
+    return table
