a4a96525016df18cc724729c0605d61f84078b20
Andre Jones <andreij6@gmail.com>
2013-11-13 01:10:02 -0600

    Web Dev Course started

--- Udacity/CompSci1/Lesson5.py
+++ Udacity/CompSci1/Lesson5.py
@@ -52,3 +52,30 @@ def better_make_hash(nbuckets):
     for unused in range(0, nbuckets):
         table.append([])
     return table
+
+
+
+def hashtable_get_bucket(htable, key):
+    return htable[hash_string(key,len(htable))]
+
+def hashtable_add(htable,key,value):
+    hashtable_get_bucket(htable,key).append([key, value])
+    return htable
+
+def hashtable_lookup(htable, key):
+    bucket = hashtable_get_bucket(htable,key)
+    for entry in bucket:
+        if entry[0] == key:
+            return entry[1]
+    return None
+
+def hashtable_update(htable, key, value):
+    bucket = hashtable_get_bucket(htable,key)
+    for entry in bucket:
+        if entry[0] == key:
+            entry[1] = value
+            return 
+    bucket.append([key, value])
+    
+    
+    
