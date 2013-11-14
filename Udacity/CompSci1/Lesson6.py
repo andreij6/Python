8f985b3f481e85c141b14cb16de1031955e21cf9
Andre Jones <andreij6@gmail.com>
2013-11-13 21:57:37 -0600

    CompSci

--- Udacity/CompSci1/Lesson6.py
+++ Udacity/CompSci1/Lesson6.py
@@ -5,3 +5,25 @@ def factorial(n):
         return n*factorial(n-1)
     
 print factorial(3)
+
+def is_palindrome(s):
+    if s == '':
+        return True
+    else:
+        if s[0] == s[-1]:
+            return is_palindrome(s[1:-1])
+        else:
+            return False
+        
+def iter_palindrome(s):
+    for i in range(0, len(s)/2):
+        if s[i] != s[-(i+1)]:
+            return False
+    return True
+
+def fibonacci(n):
+    if n==0 or n==1:
+        return n
+    return fibonacci(n-1) + fibonacci(n-2)
+    
+print fibonacci(15)
